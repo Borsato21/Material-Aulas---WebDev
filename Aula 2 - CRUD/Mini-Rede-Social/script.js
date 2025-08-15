42d07449485ca91a00d3273fee589fa68644acdb
tomasbraga-c <tomasabcantuaria@gmail.com>
2025-08-14 22:33:37 -0300

    aula dia 14/08

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,67 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+window.onload = function(){
+    mostrarPosts();
+
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+//CREATE
+function addPost(evento){
+    evento.preventDefault();
+    const textoPost = document.querySelector("#postText").value
+    const categoriaPost = document.querySelector("#postCategory").value
+    const imagemPost = document.querySelector("#postImage").value
+
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: new Date().toLocaleString()
+    }
+    posts.unshift(novoPost)
+    mostrarPosts()
+}
+
+//READ
+function mostrarPosts(){
+    //Pegando elemento aondes os tt serao inseridos 
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ""
+// passando pelo array criando um tt pra cada um 
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+            <h2>${pegaItem.text}</h2>
+            <img src = "${pegaItem.image}"/>      
+            <p>${pegaItem.date}</p>
+            <p>${pegaItem.category}</p>
+            <button>Editar</button>
+            <button>Deletar</button>
+        `
+        //add cada card ao html
+        listaPosts.append(cardPost)
+    })
+}
+//UPDATE
+function editarPosts(){
+
+}
+//DELETE
+function deletarPosts(){
+
+}
+
+// const pessoa = {
+//     nome: "Tomás",
+//     idade: 20,
+//     "Empresas trabalhadas": [],
+//     isAdmin: true
+
+// }
+
+// console.log(pessoa.nome)
+// console.log(pessoa[idade])
