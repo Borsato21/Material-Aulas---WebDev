f30fbc646e79ad5e625a38658ee5c67e1d2bcc58
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-25 22:36:04 -0300

    sdhfj

--- Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
+++ Aula 6 - APIs, Hooks e Deploy/react-dashboard/src/pages/Relatorios.jsx
@@ -3,11 +3,17 @@ import { GraficoBarras } from "@/components/GraficoBarras";
 import { GraficoPizza } from "@/components/GraficoPizza";
 
 export default function Relatorios() {
+
+  function handleClick(){
+    alert("Clicou!")
+  }
   return (
     <div className="p-6 flex-1 bg-gray-100">
       <h1 className="text-2xl font-bold">Relatórios</h1>
       <p className="mt-2 text-gray-600">Página de relatórios.</p>
-      <Button className={'cursor-pointer'}>Click me</Button>
+      <br />
+      <Button onClick={handleClick} >Click me</Button>
+      <br /><br />
       <div className="grid grid-cols-2">
         <GraficoBarras></GraficoBarras>
         <GraficoPizza></GraficoPizza>
